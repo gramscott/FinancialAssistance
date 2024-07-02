5c00c3ac63fd9146bd67d638c60b34a202de9689
gramscott <120787031+gramscott@users.noreply.github.com>
2024-07-02 15:56:09 +0100

    first commit

---
+++ src/main/java/Social_Security_Interview/FinancialAssistance/FinancialAssistanceApplication.java
@@ -0,0 +1,13 @@
+package Social_Security_Interview.FinancialAssistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAssistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Social_Security_Interview/FinancialAssistance/FinancialAssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package Social_Security_Interview.FinancialAssistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAssistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
