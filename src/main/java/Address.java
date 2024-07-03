4d3ea93d1bf7de054044893a10bca201ade41c4b
gramscott <120787031+gramscott@users.noreply.github.com>
2024-07-03 17:27:20 +0100

    Gave bank account and address some props

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,4 +1,37 @@
 public class Address {
 
+    private String street;
+    private String city;
+    private String postcode;
 
+
+    public Address(String street, String city, String postcode) {
+        this.street = street;
+        this.city = city;
+        this.postcode = postcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
 }
--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,4 +1,26 @@
 public class BankAccount {
 
+    private String accountNumber;
+    private String sortCode;
 
+    public BankAccount(String accountNumber, String sortCode) {
+        this.accountNumber = accountNumber;
+        this.sortCode = sortCode;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(String sortCode) {
+        this.sortCode = sortCode;
+    }
 }
